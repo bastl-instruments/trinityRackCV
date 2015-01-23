41a1e6ae1d37912cda8b8fb216ab79ee60a557e8
Václav Peloušek <info@bastl-instruments.com>
2015-01-23 16:14:41 +0100

    getting ready to connect interface

--- trinityRackCV.ino
+++ trinityRackCV.ino
@@ -17,9 +17,21 @@ int main(void) {
 #endif
 
 #include <portManipulations.h>
+#include <lfoExtended.h>
+#include <simpleSerialDecoder.h>
 #include "trinityRackCV_HW.h"
 #include "ADSR.h"
 ADSR envelope[6];
+lfoExtended<2000> LFO[6];
+simpleSerialDecoder com;
+
+/* channel TYPES
+ *lfo
+ *envelope
+ *cv follow
+ *interpolate
+ *trigger
+ */
 
 extern trinityRackCV_HW hardware;
 
@@ -131,20 +143,91 @@ void doMultStep(){
 }
 
 
+#define NUMBER_OF_CHANNELS 6
+#define NUMBER_OF_VALUES 3
+uint8_t channelMode[NUMBER_OF_CHANNELS];
+uint8_t channelValue[NUMBER_OF_CHANNELS][NUMBER_OF_VALUES];
 
 
 
 
+void clockCall(uint8_t number){
 
+}
+void stepCall(uint8_t number){
 
-void clockCall() {
+}
+void gateCall(uint8_t number){
 
+}
+void triggerCall(uint8_t number){
 
+}
+void startCall(uint8_t number){
+
+}
+void stopCall(uint8_t number){
+
+}
+void restartCall(uint8_t number){
+
+}
+void channelTriggerCall(uint8_t channel,uint8_t number){
+
+}
+void channelGateCall(uint8_t channel,uint8_t number){
+	if(number==0) hardware.setDAC(channel,number);
+	else hardware.setDAC(channel,255);
+}
+void channelCVCall(uint8_t channel,uint8_t number){
+	 hardware.setDAC(channel,number);
+}
+void channelInterpolateFromCall(uint8_t channel,uint8_t number){
+
+}
+void channelInterpolateToCall(uint8_t channel,uint8_t number){
+
+}
+void channeInterpolateCall(uint8_t channel,uint8_t number){
+
+}
+void channelModeCall(uint8_t channel,uint8_t number){
+	channelMode[channel]=number;
+	reflectModeChange(channel);
+}
+
+void channelValueCall(uint8_t channel, uint8_t value, uint8_t number){
+	channelValue[channel][value]=number;
+	reflectValueChange(channel,value);
+	/*
+	 if(number>128) com.sendChannelGate(channel,1);
+	 else com.sendChannelGate(channel,0);
+	 hardware.setDAC(channel,number);
+	 */
 }
 
 
 void setup() {
-	Serial.begin(9600);
+	//create callbacks
+	com.init(9600);
+	com.attachClockCallback(&clockCall);
+	com.attachStepCallback(&stepCall);
+	com.attachGateCallback(&gateCall);
+	com.attachTriggerCallback(&triggerCall);
+	com.attachStartCallback(&startCall);
+	com.attachStopCallback(&stopCall);
+	com.attachRestartCallback(&restartCall);
+	com.attachChannelTriggerCallback(&channelTriggerCall);
+	com.attachChannelGateCallback(&channelGateCall);
+	com.attachChannelCVCallback(&channelCVCall);
+	com.attachChannelInterpolateFromCallback(&channelInterpolateFromCall);
+	com.attachChannelInterpolateToCallback(&channelInterpolateToCall);
+	com.attachChannelInterpolateCallback(&channeInterpolateCall);
+	com.attachChannelModeCallback(&channelModeCall);
+	com.attachChannelValueCallback(&channelValueCall);
+
+
+	//Serial.begin(9600);
 	hardware.init(&clockCall);
 	for(int i=0;i<6;i++){
 	envelope[i].setAttackRate(1000);
@@ -153,12 +236,46 @@ void setup() {
 	envelope[i].setSustainLevel(0.5);
 	envelope[i].setTargetRatioA(0.5);
 	envelope[i].setTargetRatioDR(0.01);
+	LFO[i].init();
+	LFO[i].setFreq(0);
 	}
 }
 uint32_t gateTime;
 bool gate[6];
+
 uint32_t _time;
+
+#define TRIGGER_MODE 1
+#define GATE_MODE 2
+#define CV_MODE 3
+#define INTERPOLATE_MODE 4
+#define LFO_MODE 5
+#define ADSR_MODE 6
+
 void loop() {
+	com.update();
+	for(int i=0;i<6;i++){
+		switch(channelMode[i]){
+		case TRIGGER_MODE:
+			break;
+		case GATE_MODE:
+			break;
+		case CV_MODE:
+			break;
+		case INTERPOLATE_MODE:
+			break;
+		case LFO_MODE:
+			break;
+		case ADSR_MODE:
+			if(hardware.getCVValue(i)>>9) gate[i]=1;
+			else gate[i]=0;
+			envelope[i].gate(gate[i]);
+			break;
+		}
+	}
+
+	/*
+	 {
 	if(_time!=hardware.getElapsedBastlCycles()){
 		for(int i=0;i<6;i++){
 				float fout=(float)255*envelope[i].process();
@@ -170,9 +287,16 @@ void loop() {
 	if((hardware.getCVValue(i)>>7) != gate[i]){
 		gate[i]=hardware.getCVValue(i)>>7;
 		envelope[i].gate(gate[i]);
+		LFO[i].step();
+		LFO[i].getCurrentValue();
 	}
+
 	}
 	_time=hardware.getElapsedBastlCycles();
+*/
+
+
+
 	/*
 	if(_time-gateTime>7000){
 		gateTime=_time;
@@ -192,5 +316,36 @@ void loop() {
 }
 
 
+void reflectModeChange(uint8_t channel){
+	switch(channelMode[channel]){
+	case TRIGGER_MODE:
+		break;
+	case GATE_MODE:
+		break;
+	case CV_MODE:
+		break;
+	case INTERPOLATE_MODE:
+		break;
+	case LFO_MODE:
+		break;
+	case ADSR_MODE:
+		break;
+	}
+}
 
-
+void reflectValueChange(uint8_t channel, uint8_t value){
+	switch(channelMode[channel]){
+	case TRIGGER_MODE:
+		break;
+	case GATE_MODE:
+		break;
+	case CV_MODE:
+		break;
+	case INTERPOLATE_MODE:
+		break;
+	case LFO_MODE:
+		break;
+	case ADSR_MODE:
+		break;
+	}
+}
